ec2826c90e200e63a36c0cc97d63fe9ba9c19e71
Amandeep Singh <amandeep.singh@softvision>
2018-06-05 09:46:52 +0530

    Initial commit V01

---
+++ interviewapp/src/test/java/com/softvision/interview/interviewapp/InterviewappApplicationTests.java
@@ -0,0 +1,16 @@
+package com.softvision.interview.interviewapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InterviewappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
