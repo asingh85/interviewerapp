d7c3fb36aca5cfb5b125d2a4cb682f9025c0dae6
Amandeep Singh <amandeep.singh@softvision>
2018-06-06 19:09:38 +0530

    deleting the old structure

--- adminService/src/main/java/com/softvision/interview/adminservice/repository/InterviewerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softvision.interview.adminservice.repository;
-
-import com.softvision.interview.adminservice.model.Interviewer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InterviewerRepository extends MongoRepository<Interviewer,String> {
-
-}
--- adminService/src/main/java/com/softvision/interview/interviewapp/repository/InterviewerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.softvision.interview.interviewapp.repository;
-
-import com.softvision.interview.interviewapp.model.Interviewer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface InterviewerRepository extends MongoRepository<Interviewer,String> {
-
-}
