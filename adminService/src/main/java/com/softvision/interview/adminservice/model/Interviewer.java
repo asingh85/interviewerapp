e11e68efac710b302cac9e4c32c78ca2722305d8
Amandeep Singh <amandeep.singh@softvision>
2018-06-07 11:31:24 +0530

    Resolved the Lombok Error

--- adminService/src/main/java/com/softvision/interview/adminservice/model/Interviewer.java
+++ adminService/src/main/java/com/softvision/interview/adminservice/model/Interviewer.java
@@ -8,6 +8,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
